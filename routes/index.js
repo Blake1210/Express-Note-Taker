13d57bbdc0ec5c434c90d7cdd46eed51c3e6c957
Blake <blakepetersont@gmail.com>
2022-12-21 14:31:18 -0700

    finished the server.js

---
+++ routes/index.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const fs = require('fs');
+const {readFile, showFile} = fs.promises
+const app = express();
--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const path = require('path');
-const api = require('./routes/index.js');
+const connection = require('./routes/index.js');
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 
 const app = express();
 
+app.use(express.static('public'));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use('/api', connection)
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('*', (req, res) => 
+res.sendFile(path.join(_dirname, '/public/notes.html'))
+);
+
+app.listen(PORT, () =>
+console.log(`App listeniing at http://localhost:${PORT}`)
+);
